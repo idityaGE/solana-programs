a6f75d1e8b1784e84152d1e7534b9b3a6dca04d3
idityaGE <am44910606@gmail.com>
2025-10-04 15:30:00 +0530

    init

---
+++ escrow/programs/escrow/src/lib.rs
@@ -0,0 +1,21 @@
+pub mod constants;
+pub mod error;
+pub mod instructions;
+pub mod state;
+
+use anchor_lang::prelude::*;
+
+pub use constants::*;
+pub use instructions::*;
+pub use state::*;
+
+declare_id!("6S1hPSwWRLqzLTv4TKfTgXAGJMqJhb4SzLoiUQ4KR8Gd");
+
+#[program]
+pub mod escrow {
+    use super::*;
+
+    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+        initialize::handler(ctx)
+    }
+}
